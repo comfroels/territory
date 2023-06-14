31682094241a0d7bc1a06df8a4e46fb2a9c96f37
Nathan White <comfroels@gmail.com>
2023-06-14 14:16:02 -0600

    add konami?

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -21,12 +21,51 @@ export const meta: V2_MetaFunction = () => {
 	];
 };
 
+const code = [
+	'ArrowUp',
+	'ArrowUp',
+	'ArrowDown',
+	'ArrowDown',
+	'ArrowLeft',
+	'ArrowRight',
+	'ArrowLeft',
+	'ArrowRight',
+	'KeyB',
+	'KeyA',
+	'Enter',
+];
+
+let konamiIndex = 0;
+
+function konamiCode(evt: KeyboardEvent) {
+	if (evt.code === code[konamiIndex]) {
+		konamiIndex++;
+		if (konamiIndex === 11) {
+			const borderElement = document.getElementById('crazy-border');
+			if (borderElement) {
+				borderElement.style.border = '10px solid black';
+				borderElement.style.borderImage =
+					"url(\"data:image/svg+xml;charset=utf-8,%3Csvg width='100' height='100' viewBox='0 0 100 100' fill='none' xmlns='http://www.w3.org/2000/svg'%3E %3Cstyle%3Epath%7Banimation:stroke 5s infinite linear%3B%7D%40keyframes stroke%7Bto%7Bstroke-dashoffset:776%3B%7D%7D%3C/style%3E%3ClinearGradient id='g' x1='0%25' y1='0%25' x2='0%25' y2='100%25'%3E%3Cstop offset='0%25' stop-color='%232d3561' /%3E%3Cstop offset='25%25' stop-color='%23c05c7e' /%3E%3Cstop offset='50%25' stop-color='%23f3826f' /%3E%3Cstop offset='100%25' stop-color='%23ffb961' /%3E%3C/linearGradient%3E %3Cpath d='M1.5 1.5 l97 0l0 97l-97 0 l0 -97' stroke-linecap='square' stroke='url(%23g)' stroke-width='3' stroke-dasharray='388'/%3E %3C/svg%3E\") 1";
+				borderElement.style.background = `-webkit-linear-gradient(45deg, #2d3561, #c05c7e, #f3826f, #ffb961)`;
+			}
+		}
+	} else {
+		konamiIndex = 0;
+	}
+}
+
 export default function Index() {
 	const [games, setGames] = React.useState([]);
 	const [gameId, setGameId] = React.useState('');
 	const { user, setUser } = useUserContext();
 	const [isRulesOpen, setIsRulesOpen] = React.useState(false);
 	const navigate = useNavigate();
+
+	React.useEffect(() => {
+		if (typeof window === 'undefined') return;
+		window.document.addEventListener('keydown', konamiCode);
+		return () => window.document.removeEventListener('keydown', konamiCode);
+	}, []);
 
 	async function newGame() {
 		const game: Game = {
@@ -109,7 +148,8 @@ export default function Index() {
 				<Typography
 					textAlign='center'
 					variant='h1'
-					fontFamily='VT323, monospace'>
+					fontFamily='VT323, monospace'
+					id='crazy-border'>
 					Territory
 				</Typography>
 			</Grid>
